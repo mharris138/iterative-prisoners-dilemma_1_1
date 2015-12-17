5deea293fe3c727422423b668adf778be5762c73
mharris138 <matthew.harris@students.sacs.k12.in.us>
2015-12-17 09:04:33 -0500

    Update prisoners_dilemma.py

--- prisoners_dilemma.py
+++ prisoners_dilemma.py
@@ -160,7 +160,7 @@ def get_action(player, history, opponent_history, score, opponent_score, getting
             return 'loyal vengeful'
         else:
             if len(opponent_history)==0: #It's the first round: collude
-                return 'b'
+                return 'c'
             elif history[-1]=='c' and opponent_history[-1]=='c':
                 return 'c' # betray if they were severely punished last time
             else:
